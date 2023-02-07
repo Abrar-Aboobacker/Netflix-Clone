88ced073616b3ee57bc0a6828e2a46ca9cab1268
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-02-07 19:28:12 +0530

    card selection done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import './app.css'
 import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
 
 function App() {
   return (
   <div>
       <NavBar />
-      <Banner/>
+      <Banner />
+      <RowPost/>
   </div>
 )
 }
